dde69bf743505ae7cf286a6de241e21686624ff8
Edward Wilde <ewilde@gmail.com>
2019-04-28 13:14:52 -0500

    Adds github scenario
    Signed-off-by: Edward Wilde <ewilde@gmail.com>

---
+++ github/content-team-codeowners.tf
@@ -0,0 +1,5 @@
+resource "github_team" "content_code_owners" {
+  name        = "Approvers"
+  description = "Members of this team have permissions to approve pull requests for the content team"
+}
+
---
+++ github/content-team.tf
@@ -0,0 +1,44 @@
+locals  {
+  content_team_members =  ["bob-ewilde", "sally-ewilde"]
+}
+
+resource "github_repository" "website" {
+  name          = "12factor.io"
+  description   = "The 12factor.io public website"
+  has_issues    = "true"
+  has_downloads = "true"
+  has_wiki      = "true"
+}
+
+// repos
+resource "github_repository" "documentation" {
+  name           = "documentation"
+  description    = "company wide awesome documentation"
+  default_branch = "master"
+}
+
+resource "github_team" "content" {
+  name        = "Content"
+  description = "Members of this team manage content within the organisation i.e. documentation, the website ..."
+}
+
+// team membership
+resource "github_team_membership" "content_team_membership" {
+  team_id  = "${github_team.content.id}"
+  username = "${local.content_team_members[count.index]}"
+  role     = "member"
+  count    = "${length(local.content_team_members)}"
+}
+
+// team permissions
+resource "github_team_repository" "team_repo_documentation" {
+  team_id    = "${github_team.content.id}"
+  repository = "${github_repository.documentation.name}"
+  permission = "push"
+}
+
+resource "github_team_repository" "team_repo_webiste" {
+  team_id    = "${github_team.content.id}"
+  repository = "${github_repository.website.name}"
+  permission = "push"
+}
